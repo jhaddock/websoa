134141aa5a23ecc160ea867d7548b8f0c7c41ee6
ariera <ariera@gmail.com>
2010-11-30 16:54:48 +0100

    commit temporal

---
+++ wp-content/themes/SOA-Theme/col-centro-header.php
@@ -0,0 +1,13 @@
+<div class="header span-11 last">
+    <ul>
+        <li><a href="#esp"><img src="<?php soa_image('flag-esp.png') ?>"/> ESP</a><br/></li>
+        <li><a href="#eng"><img src="<?php soa_image('flag-eng.png') ?>"/> ENG</a></li>
+    </ul>
+    <div class="hr fatline"></div>
+    <ul class="menu">
+        <li><a href="#">Azafatas</a> / </li>
+        <li><a href="#">Modelos</a> / </li>
+        <li><a href="#">Conductores</a> / </li>
+        <li><a href="#">Otros</a></li>
+    </ul>
+</div><!-- header -->
