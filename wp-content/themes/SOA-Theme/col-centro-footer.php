40e8249fff0446e4e32318850bc60b12838e7cde
ariera <ariera@gmail.com>
2010-12-02 03:39:07 +0100

    fixed a problem with the footer

--- wp-content/themes/SOA-Theme/col-centro-footer.php
+++ wp-content/themes/SOA-Theme/col-centro-footer.php
@@ -1,6 +1,3 @@
-        <div class="footer">
-            <div class="hr thinline"></div>
-            <p>Julian Romea 5, entreplanta D. 28003 Madrid. info@soastaff.com <em>Tel +34 915 357 651</em></p>
-        </div>
+
     </div><!-- contents -->
 </div><!-- col_centro -->
--- wp-content/themes/SOA-Theme/footer.php
+++ wp-content/themes/SOA-Theme/footer.php
@@ -1,5 +1,10 @@
      </div><!-- container -->
     </div>
-    <div class="bottom"></div>
+    <div class="bottom">
+        <div class="footer">
+            <div class="hr thinline"></div>
+            <p>Julian Romea 5, entreplanta D. 28003 Madrid. info@soastaff.com <em>Tel +34 915 357 651</em></p>
+        </div>
+    </div>
   </body>
 </html>
