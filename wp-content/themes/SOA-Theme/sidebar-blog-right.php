3c2d7ee5ff1b5fb4b13d6b6c315f1142e4dbcffc
ariera <ariera@gmail.com>
2010-12-22 10:16:56 +0100

    mini fixes y archive por fechas

--- wp-content/themes/SOA-Theme/sidebar-blog-right.php
+++ wp-content/themes/SOA-Theme/sidebar-blog-right.php
@@ -1,6 +1,36 @@
 <div id="col-derecha" class="span-5 last columna">
+
+    <ul class="archive by_year">
+    <?php
+    /**/
+    $years = $wpdb->get_col("SELECT DISTINCT YEAR(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY post_date DESC");
+    foreach($years as $year) :
+    ?>
+            <li class="first">Eventos:</li>
+            <li><a href="<?php echo get_year_link($year); ?> "><?php echo $year; ?></a>
+
+                    <ul class="by_month">
+                    <?	$months = $wpdb->get_col("SELECT DISTINCT MONTH(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' AND YEAR(post_date) = '".$year."' ORDER BY post_date DESC");
+                            foreach($months as $month) :
+                            ?>
+                            <li><a href="<?php echo get_month_link($year, $month); ?>"><?php echo date( 'F', mktime(0, 0, 0, $month) );?></a>
+                                    <ul class="by_day">
+                                    <?	$days = $wpdb->get_col("SELECT DISTINCT DAY(post_date) FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = 'post' AND MONTH(post_date) = '".$month."' AND YEAR(post_date) = '".$year."' ORDER BY post_date DESC");
+                                            foreach($days as $day) :
+                                            ?>
+                                            <li><a href="<?php echo get_day_link($year, $month, $day); ?>"><?php echo $day;?></a></li>
+                                            <?php endforeach;?>
+                                    </ul>
+
+                            </li>
+                            <?php endforeach;?>
+                    </ul>
+            </li>
+    <?php endforeach; ?>
+    </ul>
+
     <ul class="wp-categories">
-        <li class="first">Cotegorías:</li>
+        <li class="first">Categorías:</li>
         <?php wp_list_categories(array('title_li'=> '', 'exclude' => '1')); ?>
     </ul>
 
